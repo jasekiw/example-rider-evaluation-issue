022b1d1ac8d2e1bcbfee6c26e793484f16c4a349
Jason Gallavin <jasongallavin@gmail.com>
2021-09-30 20:54:25 -0400

    first commit

---
+++ ExampleRiderEvaluationIssue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sentry;
+
+namespace ExampleRiderEvaluationIssue
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception e)
+            {
+                SentrySdk.WithScope(scope =>
+                {
+                    SentrySdk.CaptureException(e);
+                });
+            }
+        }
+    }
+}
